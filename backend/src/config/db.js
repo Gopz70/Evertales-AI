44d7f00a5046a35d6be8df1caf3979c9e1beeb5c
Gopz70 <mgopikrishnan70@gmail.com>
2026-09-12 15:19:15 +0530

    Bug_Fix - Evertales AI

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,5 +11,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
-
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
 module.exports = pool;
